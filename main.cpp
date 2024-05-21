d4cc6edcee62e582c6bc0b0d5ac5d4cebca651a6
Filip Ignaciuk <250965@edu.p.lodz.pl>
2024-05-21 21:06:23 +0200

    queue

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "queue.h"
+
+void menu(void)
+{
+    printf("\n");
+    printf("1 - Dodaj liczbe do kolejki (Add)\n");
+    printf("2 - Odczytaj pierwsza wartosc z kolejki (First)\n");
+    printf("3 - Usun wartosc z z kolejki(Remove)\n");
+    printf("4 - Sprawdz czy kolejka jest pusta\n");
+    printf("5 - Sprawdz czy kolejka jest pelna\n");
+    printf("6 - Koniec programu\n");
+    printf("\n");
+}
+
+int main()
+{
+    int temp = 0;
+    int option = 0;
+    printf("Kolejka - implementacja w tablicy statycznej\n");
+    while (1)
+    {
+        menu();
+        scanf("%d", &option);
+
+
+        switch (option)
+        {
+            case 1:
+                if ( !isQueueFull() ){
+                    printf("Podaj wartosc: ");
+                    scanf("%d", &temp);
+                    add(temp);
+                }
+                else {
+                    printf("operacja niedozwolona Kolejka pelna!!!\n\n");
+                }
+
+                break;
+
+            case 2:
+                if (!isQueueEmpty()) {
+                    temp = first();
+                    printf("Odczytana wartosc: %d", temp);
+                }
+                else {
+                    printf("operacja niedozwolona Kolejka pusta!!!\n\n");
+                }
+
+                break;
+
+            case 3:
+                if (!isQueueEmpty()) {
+                    temp = remove();
+                    printf("Odczytana wartosc: %d", temp);
+                }
+                else {
+                    printf("operacja niedozwolona Kolejka pusta!!!\n\n");
+                }
+                break;
+
+            case 4:
+                if (isQueueEmpty()) {
+                    printf("Kolejka jest pusta.\n");
+                }
+                else {
+                    printf("Kolejka nie jest pusta.\n");
+                }
+                break;
+
+            case 5:
+                if (isQueueFull()) {
+                    printf("Kolejka jest pelna.\n");
+                }
+                else {
+                    printf("Kolejka nie jest pelna.\n");
+                }
+                break;
+
+            case 6:
+                //zakonczenie programu
+                return 0;
+
+            default:
+                printf("Wybierz wlasciwa opcje.\n\n");
+                break;
+        }
+    }
+    return 0;
+}
+
---
+++ queue.h
@@ -0,0 +1,10 @@
+#ifndef QUEUE_QUEUE_H
+#define QUEUE_QUEUE_H
+
+int isQueueEmpty(void);
+int isQueueFull(void);
+int remove(void);
+int first(void);
+void add(int val);
+
+#endif //QUEUE_QUEUE_H
